81c5668f8d6ab3abd69c18691ac52ddd6fd1dd16
shahbaz-alam7 <alamshahbaz287@gmail.com>
2023-09-06 00:17:10 +0530

    scroll view

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+const logoImage = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <Text>Hello World</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "plum",
+        paddingHorizontal: 20,
+        paddingVertical: 30,
+      }}
+    >
+      <ScrollView>
+        <Image
+          source={{ uri: "https://picsum.photos/200" }}
+          style={{ width: 300, height: 300 }}
+        />
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+          asperiores, fugiat minus nesciunt pariatur dolorum vero autem dicta,
+          commodi dolores repellendus quas praesentium consequuntur quis
+          provident error! Accusantium quas, omnis explicabo nesciunt minima eum
+          adipisci non ut doloremque quos consequatur, dolorum, minus optio
+          voluptatibus fugit dolor commodi cum corporis autem enim earum natus
+          vero est accusamus? Fugiat dolor error reprehenderit. Autem, ullam
+          ratione nemo voluptatem ducimus amet, odit repellat perspiciatis
+          expedita quod aliquid laboriosam, esse officia fugit. Hic recusandae
+          eos deserunt quia. Ex nemo tempore, perferendis voluptatem numquam
+          totam veritatis hic nobis autem doloremque, aspernatur in, vitae culpa
+          animi unde illum? Voluptas omnis amet aliquid quas nisi possimus alias
+          doloribus minima, accusamus fuga maxime deleniti praesentium, magni
+          beatae ducimus, libero inventore veritatis cum!
+        </Text>
+        <Image
+          source={{ uri: "https://picsum.photos/200" }}
+          style={{ width: 300, height: 300 }}
+        />
+      </ScrollView>
     </View>
   );
 }
