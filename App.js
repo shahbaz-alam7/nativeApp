70b6c7eb94e86eed503fc2c57690573b96fc9bdc
shahbaz-alam7 <alamshahbaz287@gmail.com>
2023-09-06 00:05:44 +0530

    base

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
-import { Image, ImageBackground, Text, View } from "react-native";
-const logoImage = require("./assets/adaptive-icon.png");
+import { Text, View } from "react-native";
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum" }}>
-      {/* <View style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}>
-        <Text style={{ padding: 20 }}>
-          Shahbaz
-          <Text style={{ color: "red" }}>Alam</Text>
-        </Text>
-      </View> */}
-      {/* <View style={{ width: 200, height: 200, backgroundColor: "lightblue" }}>
-        <Image source={logoImage} style={{ width: 300, height: 300 }} />
-        <Image
-          source={{ uri: "https://picsum.photos/200" }}
-          style={{ width: 300, height: 300 }}
-        />
-      </View> */}
-      {/* <View> */}
-      <ImageBackground source={logoImage} style={{ flex: 1 }}>
-        <Text>Shahbaz Abc</Text>
-      </ImageBackground>
-      {/* </View> */}
+      <Text>Hello World</Text>
     </View>
   );
 }
