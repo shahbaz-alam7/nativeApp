0819b9c8f2fe42c55effe5ac39985e79748255c2
shahbaz-alam7 <alamshahbaz287@gmail.com>
2023-09-06 00:07:00 +0530

    text

--- App.js
+++ App.js
@@ -2,7 +2,12 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <Text>Hello World</Text>
+      <View style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}>
+        <Text style={{ padding: 20 }}>
+          Shahbaz
+          <Text style={{ color: "red" }}>Alam</Text>
+        </Text>
+      </View>
     </View>
   );
 }
