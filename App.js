ed88fa900f35e5f2047c9bc0e3e869be2bda9c3a
shahbaz-alam7 <alamshahbaz287@gmail.com>
2023-09-13 00:51:58 +0530

    activity indicator

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View, ActivityIndicator } from "react-native";
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <Text>Hello World</Text>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="large" color="black" />
+      <ActivityIndicator size="large" color="black" animating={false} />
     </View>
   );
 }
