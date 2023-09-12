c3c0bb4d695c67c75d62370b1d4376ed262af5ee
shahbaz-alam7 <alamshahbaz287@gmail.com>
2023-09-13 00:35:02 +0530

    Modal

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, View, Modal, Button } from "react-native";
 export default function App() {
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <Text>Hello World</Text>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Button title="Open" onPress={() => setOpenModal(true)} />
+      <Modal
+        visible={openModal}
+        animationType="slide"
+        onRequestClose={() => setOpenModal(false)}
+        presentationStyle="formSheet"
+      >
+        {/* animationType="slide" || "fade" */}
+        <Text>Shahbaz</Text>
+        <Button onPress={() => setOpenModal(false)} title="Close" />
+      </Modal>
     </View>
   );
 }
