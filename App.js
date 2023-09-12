b07195499e4b2ffecb4a2c5fee6e9f4606193684
shahbaz-alam7 <alamshahbaz287@gmail.com>
2023-09-13 01:07:57 +0530

    alert

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
-import { Text, View } from "react-native";
+import { Text, View, Button, Alert } from "react-native";
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <Text>Hello World</Text>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Button title="Press" onPress={() => Alert.alert("Invalid data")} />
+      <Button
+        title="Press2"
+        onPress={() =>
+          Alert.alert("Invalid data", "DOB is not in proper format")
+        }
+      />
+      <Button
+        title="Press3"
+        onPress={() =>
+          Alert.alert("Invalid data!", "Please enter valid data", [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel button is pressed"),
+            },
+            {
+              text: "OK",
+              onPress: () => console.log("OK button is pressed"),
+            },
+          ])
+        }
+      />
     </View>
   );
 }
